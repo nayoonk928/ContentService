61175e86e702aa7a6319816440f9e6aec7a474f7
Nayoon <nayoonk928@gmail.com>
2023-07-23 23:00:32 +0900

    refactor: change email regex

--- src/main/java/com/personal/contentservice/util/EmailValidator.java
+++ src/main/java/com/personal/contentservice/util/EmailValidator.java
@@ -4,7 +4,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<EmailValidation, String> {
-  private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+  private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
 
   @Override
   public void initialize(EmailValidation constraintAnnotation) {
