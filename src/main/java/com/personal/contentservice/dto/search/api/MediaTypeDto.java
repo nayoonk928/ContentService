aa9e1a512f6eb579d689d5527acdb554df13594c
Nayoon <nayoonk928@gmail.com>
2023-08-06 01:17:21 +0900

    refactor: Refactor search DTOs and service
    - api통신 dto와 service dto 분리
    - dto에 맞춰 service 코드와 test 코드 수정

--- src/main/java/com/personal/contentservice/dto/search/api/MediaTypeDto.java
+++ src/main/java/com/personal/contentservice/dto/search/api/MediaTypeDto.java
@@ -2,9 +2,9 @@ package com.personal.contentservice.dto.search.api;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.personal.contentservice.dto.search.SearchContentDto;
-import java.util.Map;
+import lombok.Getter;
 
+@Getter
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
     include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "media_type", visible = true)
 @JsonSubTypes({
@@ -13,10 +13,6 @@ import java.util.Map;
     @JsonSubTypes.Type(value = SearchPersonResponse.class, name = "person")
 })
 public abstract class MediaTypeDto {
-
-  public void convertGenreIdsToNames(Map<Long, String> genreMap){}
-
-  public SearchContentDto toSearchContentDto(){return null;}
 
 }
 
--- src/main/java/com/personal/contentservice/dto/search/api/SearchTvResponse.java
+++ src/main/java/com/personal/contentservice/dto/search/api/SearchTvResponse.java
@@ -1,10 +1,7 @@
 package com.personal.contentservice.dto.search.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.personal.contentservice.dto.search.SearchContentDto;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,39 +11,9 @@ import lombok.NoArgsConstructor;
 public class SearchTvResponse extends MediaTypeDto {
 
   private long id;
-
   private String name;
-
   private String mediaType;
-
   private List<Long> genreIds;
-
   private String firstAirDate;
-
-  private List<String> genreNames;
-
-  @Override
-  public void convertGenreIdsToNames(Map<Long, String> genreMap) {
-    if (genreIds != null) {
-      genreNames = new ArrayList<>();
-      for (long id : genreIds) {
-        String name = genreMap.get(id);
-        if (name != null) {
-          genreNames.add(name);
-        }
-      }
-    }
-  }
-
-  @Override
-  public SearchContentDto toSearchContentDto() {
-    return SearchContentDto.builder()
-        .id(id)
-        .title(name)
-        .mediaType(mediaType)
-        .genreNames(genreNames)
-        .date(firstAirDate)
-        .build();
-  }
 
 }
